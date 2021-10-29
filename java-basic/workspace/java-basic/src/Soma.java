99cd356d74ba7b45c3ccd27b0cc976158ffec59d
João Guilherme <71348300+jottabatista@users.noreply.github.com>
2021-10-29 20:26:44 -0300

    Update Soma.java
    about System.ou.println, Scanner, and int

--- java-basic/workspace/java-basic/src/Soma.java
+++ java-basic/workspace/java-basic/src/Soma.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
+import javax.swing.JSpinner;
+
 public class Soma {
+
+	private static double scanner;
 
 	public static void main(String[] args) {
 		// Vc tem que ter duas variaveis inteiras pra receber o valor digitado no teclado
@@ -7,6 +13,25 @@ public class Soma {
 		// Vc vai precisar do sysout para imprimir o resultado
 		
 		// Depois vc vai ter que criar a classe Subtracao, Multiplicacao, Divisao e fazer o mesmo
-	}
-
+		
+		
+		int primeiroNumero = (0);
+		int segundoNumero = (0);
+		int somaTotal = (primeiroNumero + segundoNumero);
+		//System.in é a informação que vem do teclado
+		Scanner s = new Scanner(System.in);
+			
+		System.out.println("Some o primeiro número");
+		primeiroNumero = s.nextInt();
+				
+		System.out.println("Some o segundo número");
+		segundoNumero = s.nextInt();
+		
+		somaTotal = primeiroNumero + segundoNumero;
+		
+		System.out.println("A soma total é " + somaTotal);
+		
+	
+		
+		}
 }
